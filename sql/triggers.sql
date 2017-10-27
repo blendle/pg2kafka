14e7411db2a344bc25a61ed6fe0ceae55a82137b
Jean Mertz <jean@mertz.fm>
2017-10-27 15:59:25 +0200

    Add configurable external_id name
    When calling `pg2kafka.setup` you now provide it with a second argument
    to define the "external_id" you want to use as the message key when
    procuding the message to Kafka.
    
    Examples:
    
        pg2kafka.setup('users', 'uuid')
        pg2kafka.setup('products', 'uid')
        pg2kafka.setup('subscriptions', 'user_uuid')

--- sql/triggers.sql
+++ sql/triggers.sql
@@ -10,11 +10,19 @@ DECLARE
   outbound_event record;
   notification json;
 BEGIN
+  SELECT pg2kafka.external_id_relations.external_id INTO external_id
+  FROM pg2kafka.external_id_relations
+  WHERE table_name = TG_TABLE_NAME;
+
   IF TG_OP = 'INSERT' THEN
-    external_id := NEW.uuid; -- TODO: uuid or uid or id?
+    EXECUTE format('SELECT ($1).%s::text', external_id) USING NEW INTO external_id;
+  ELSE
+    EXECUTE format('SELECT ($1).%s::text', external_id) USING OLD INTO external_id;
+  END IF;
+
+  IF TG_OP = 'INSERT' THEN
     changes := row_to_json(NEW);
   ELSIF TG_OP = 'UPDATE' THEN
-    external_id := OLD.uuid;
     changes := row_to_json(NEW);
     -- Remove object that didn't change
     FOR col IN SELECT * FROM jsonb_each(row_to_json(OLD)::jsonb) LOOP
@@ -23,7 +31,6 @@ BEGIN
       END IF;
     END LOOP;
   ELSIF TG_OP = 'DELETE' THEN
-    external_id := OLD.uuid;
     changes := '{}'::jsonb;
   END IF;
 
@@ -43,7 +50,7 @@ BEGIN
 END
 $_$;
 
-CREATE OR REPLACE FUNCTION pg2kafka.create_snapshot_events(table_name regclass) RETURNS void
+CREATE OR REPLACE FUNCTION pg2kafka.create_snapshot_events(table_name_ref regclass) RETURNS void
 LANGUAGE plpgsql
 AS $_$
 DECLARE
@@ -52,19 +59,22 @@ DECLARE
   changes jsonb;
   external_id varchar;
 BEGIN
-  query := 'SELECT * FROM ' || table_name;
+  SELECT pg2kafka.external_id_relations.external_id INTO external_id
+  FROM pg2kafka.external_id_relations
+  WHERE pg2kafka.external_id_relations.table_name = table_name_ref::varchar;
+
+  query := 'SELECT * FROM ' || table_name_ref;
 
   FOR rec IN EXECUTE query LOOP
-    external_id := rec.uuid; -- TODO: uuid / uid / id
     changes := json_strip_nulls(row_to_json(rec));
 
-    INSERT INTO pg2kafka.outbound_event_queue(external_id, table_name, statement, data)
-    VALUES (external_id, table_name, 'SNAPSHOT', changes);
+    INSERT INTO pg2kafka.outbound_event_queue(external_id, table_name_ref, statement, data)
+    VALUES (external_id, table_name_ref, 'SNAPSHOT', changes);
   END LOOP;
 END
 $_$;
 
-CREATE OR REPLACE FUNCTION pg2kafka.setup(table_name regclass) RETURNS void
+CREATE OR REPLACE FUNCTION pg2kafka.setup(table_name_ref regclass, external_id_name text) RETURNS void
 LANGUAGE plpgsql
 AS $_$
 DECLARE
@@ -72,17 +82,20 @@ DECLARE
   lock_query varchar;
   trigger_query varchar;
 BEGIN
-  trigger_name := table_name || '_enqueue_event';
-  lock_query := 'LOCK TABLE ' || table_name || ' IN ACCESS EXCLUSIVE MODE';
+  INSERT INTO pg2kafka.external_id_relations(external_id, table_name)
+  VALUES (external_id_name, table_name_ref);
+
+  trigger_name := table_name_ref || '_enqueue_event';
+  lock_query := 'LOCK TABLE ' || table_name_ref || ' IN ACCESS EXCLUSIVE MODE';
   trigger_query := 'CREATE TRIGGER ' || trigger_name
-    || ' AFTER INSERT OR DElETE OR UPDATE ON ' || table_name
+    || ' AFTER INSERT OR DElETE OR UPDATE ON ' || table_name_ref
     || ' FOR EACH ROW EXECUTE PROCEDURE pg2kafka.enqueue_event()';
 
   -- We aqcuire an exlusive lock on the table to ensure that we do not miss any
   -- events between snapshotting and once the trigger is added.
   EXECUTE lock_query;
 
-  PERFORM pg2kafka.create_snapshot_events(table_name);
+  PERFORM pg2kafka.create_snapshot_events(table_name_ref);
 
   EXECUTE trigger_query;
 END
